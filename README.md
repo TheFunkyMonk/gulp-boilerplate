53dafb012777ce18f42798ad3aa167a4890f140e
rlacount <rlacount@deloitte.com>
2014-04-09 11:06:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gulp Boilerplate
+
+Personal boilerplate setup for projects, using [gulp.js](http://gulpjs.com/), [HTML5 Boilerplate](http://html5boilerplate.com/), and my preferences for pre-processors and directory structure.
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    compass = require('gulp-compass'),
+    concat = require('gulp-concat'),
+    jade = require('gulp-jade'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
+
+var paths = {
+    sass: { src:'components/sass/main.scss', dst: 'css/' },
+    jade: { src: 'components/jade/*.jade', dst: './' },
+    mainJS: { src: 'components/js/main.js' },
+    allJS: { src: ['components/js/lib/*.js', 'components/js/*.js'], dst: 'js/' }
+};
+
+gulp.task('sass', function() {
+  gulp.src(paths.sass.src)
+    .pipe(compass({
+      css: 'css',
+      sass: 'components/sass',
+      style: 'expanded'
+    }));
+});
+
+gulp.task('html', function() {
+  gulp.src(paths.jade.src)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(paths.jade.dst));
+});
+
+gulp.task('js', function() {
+  gulp.src(paths.mainJS.src)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+  gulp.src(paths.allJS.src)
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(paths.allJS.dst));
+});
+
+gulp.task('default', ['sass', 'html', 'js'], function() {
+  var server = livereload();
+  gulp.watch(paths.sass.src, ['sass']);
+  gulp.watch(paths.jade.src, ['html']);
+  gulp.watch(paths.mainJS.src, ['js']);
+  gulp.watch(['*.html', 'css/*', 'js/*'], function(e) {
+    server.changed(e.path);
+  });
+});
